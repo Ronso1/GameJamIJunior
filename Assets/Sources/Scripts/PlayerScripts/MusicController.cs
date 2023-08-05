e278c0f610c745a660146d2ec0a4b24d84853eac
Ivan <gureff.al@yandex.ru>
2023-08-05 18:03:49 +0300

    Added sounds for player, created script MusicController.cs

---
+++ Assets/Sources/Scripts/PlayerScripts/MusicController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] private AudioSource _playerSource;
+    [SerializeField] private AudioClip _walking;
+    private bool _musicChecker = false;
+    private void Update()
+    {
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D)) _musicChecker = false;
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && _musicChecker == false)
+        {
+            _playerSource.enabled = true;
+            print("ss");
+            _playerSource.Play();
+            _musicChecker = true;
+        }
+        else if (_musicChecker == false) _playerSource.enabled = false;
+    }
+}
