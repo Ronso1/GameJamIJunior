39f17a10d63d0905e03e7e6d699d383f98e0ef91
Ivan <gureff.al@yandex.ru>
2023-08-06 14:28:29 +0300

    Added in game pause

--- Assets/Sources/Scripts/MenuScripts/MenuFunctions.cs
+++ Assets/Sources/Scripts/MenuScripts/MenuFunctions.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuFunctions : MonoBehaviour
 {
+    [SerializeField] private GameObject _cameraPlayer;
+    [SerializeField] private GameObject _menu;
     public void CloseGame() => Application.Quit();
     public void LoadGame() => SceneManager.LoadScene("GameScene");
+    public void ResumeGame()
+    {
+        _menu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        _cameraPlayer.GetComponent<CameraMovement>().enabled = true;
+        Time.timeScale = 1;
+    }
+    public void ExitToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MenuScene");
+    }
 }
--- Assets/Sources/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Sources/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,11 +5,21 @@ public class PlayerMovement : MonoBehaviour
     private const string VerticalAxis = "Vertical";
     [SerializeField] private Rigidbody _rigidBody;
     [SerializeField] private Transform _root;
+    [SerializeField] private GameObject _camera;
     [SerializeField] private float _speed;
+    [SerializeField] private Canvas _canvas;
     private float _horizontal;
     private float _vertical;
     private void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            GamePause();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            _camera.GetComponent<CameraMovement>().enabled = false;
+            _canvas.gameObject.SetActive(true);
+        }      
         _horizontal = Input.GetAxis(HorizontalAxis);
         _vertical = Input.GetAxis(VerticalAxis);
     }
@@ -21,4 +31,5 @@ public class PlayerMovement : MonoBehaviour
         else
             _rigidBody.MovePosition(_rigidBody.transform.position + moveDirection * _speed * Time.fixedDeltaTime);
     }
+    private void GamePause() => Time.timeScale = 0f;
 }
