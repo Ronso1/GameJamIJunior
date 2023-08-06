f1e256dd1f6bbec9b3f7cbed6f44974f1bae17a3
Ivan <gureff.al@yandex.ru>
2023-08-06 17:13:55 +0300

    Added sign and AI impovments

--- Assets/Sources/Scripts/EnemyScript/EnemyAI.cs
+++ Assets/Sources/Scripts/EnemyScript/EnemyAI.cs
@@ -4,12 +4,17 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent _enemy;
     [SerializeField] private GameObject _player;
+    [SerializeField] private Animator _enemyAnim;
+    [SerializeField] private GameObject _goodCube;
     private void Update()
     {
         if (_enemy.remainingDistance <= 0.9f && _enemy.remainingDistance != Mathf.Infinity && _enemy.remainingDistance != 0f)
         {
             print(_enemy.remainingDistance);
             _enemy.isStopped = true;
+            _enemyAnim.SetBool("IsHolding", true);
+            Instantiate(_goodCube);
+
         }        
         else
             _enemy.SetDestination(_player.transform.position);
