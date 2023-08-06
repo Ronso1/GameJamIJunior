f75421b270e397abeaf2e2460a5d7f26fa495dc4
Ivan <gureff.al@yandex.ru>
2023-08-06 16:51:05 +0300

    Reupload AI script

--- Assets/Sources/Scripts/EnemyScript/EnemyAI.cs
+++ Assets/Sources/Scripts/EnemyScript/EnemyAI.cs
@@ -4,5 +4,14 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent _enemy;
     [SerializeField] private GameObject _player;
-    private void Update() => _enemy.SetDestination(_player.transform.position);
+    private void Update()
+    {
+        if (_enemy.remainingDistance <= 0.9f && _enemy.remainingDistance != Mathf.Infinity && _enemy.remainingDistance != 0f)
+        {
+            print(_enemy.remainingDistance);
+            _enemy.isStopped = true;
+        }        
+        else
+            _enemy.SetDestination(_player.transform.position);
+    }
 }
